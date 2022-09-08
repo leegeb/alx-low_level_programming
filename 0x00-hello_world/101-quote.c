e9d63ff22b5557c6411243517de3d3baf49bafdd
lydia <lydiakgebre@gmail.com>
2022-09-08 16:47:29 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
 # include <stdio.h>
-# include <unistd.h>
 /**
- * main - prints out last part of a quote in the standard error
- * Description:  A file 101-quote.c
- * Return : 1 is always success
+ * main - Entry point
+ * Return: Always 1 (Success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19/n", 59);
 	return (1);
 }
