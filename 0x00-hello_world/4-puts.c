115f254f09e087e905a6af08094896a215aafd9c
lydia <lydiakgebre@gmail.com>
2022-09-08 08:32:00 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * main - print out a string to stdout
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
