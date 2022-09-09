5a71f8bc8e487465e8566fdd1cdc4f37ee46560c
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-09 22:03:08 +0400

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: Prints the alphabet in lowercase
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+        char alphabet;
+
+        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+                putchar(alphabet);
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+		putchar(alphabet);
+
+	putchar('\n');
+
+        return (0);
+}
+
