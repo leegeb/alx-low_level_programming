561013b048fdba6f21f543d9d24fd13f0d7688bf
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-10 02:25:45 +0400

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,15 +7,13 @@
  */
 int main(void)
 {
-	int digit1 = 0;
-	int digit2 = 1;
-	int digit3 = 2;
+	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (digit1 += 1; digit2 < 10; digit2++)
-	{
-			for (digit3 = digit2 + 1; digit3 < 11; digit3++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
@@ -23,7 +21,6 @@ int main(void)
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
-
 				putchar(',');
 				putchar(' ');
 			}
