7b834db83cead6ea79398a45ef8ac6e11a61b171
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-09 21:06:11 +0400

    task 1

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 5)
+	if (n < 6)
 		printf("Last digit of n is %d and is less than 6 and not 0,\n", n);
 	else if (n > 5)
 		printf("Last digit of n is  %d and is greater than 5,\n", n);
