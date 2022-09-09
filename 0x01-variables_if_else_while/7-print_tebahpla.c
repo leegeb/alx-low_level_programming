e80a0db5cab2c0f625e93cbdf734c7cfe68b48b3
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-09 23:07:19 +0400

    task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+ * main - entry option
+ * Description: prints the lowercase alphabet in reverse
+ * Return: 0 is success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
