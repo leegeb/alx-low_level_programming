6adc56329fa147d41aee84a8c57b62d41e20339a
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-09 22:52:16 +0400

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints all single digit numbers of base 10 starting 0
+ * Return: 0 is success
+ */
+int main(void)
+
+{
+
+	int n;
+
+	for (n = 0; n < 0; n++)
+		putchar((n % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
