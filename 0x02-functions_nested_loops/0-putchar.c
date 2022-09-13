43bf938ab58397e45cb7c17dceafc8225ea93b1a
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-13 22:34:59 +0400

    task 0 updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-# include "main.h"
+#include "main.h"
 /**
- * main - Entry point 
- * Description: prints _putchar followed by a new line 
+ * main - Entry point
+ * Description: prints _putchar followed by a new line
  * Return: 0 is success
  */
 int main(void)
 {
-	char *sh = *_putchar"
-	
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
