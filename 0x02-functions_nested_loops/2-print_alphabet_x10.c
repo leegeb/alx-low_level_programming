495630e1b9ae0fb5edcd40bedd31e3269a2ac6e8
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-13 23:07:13 +0400

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10(void) -> prints 10 times the alphabet in lower case
+ */
+void print_alphabet_x10(void);
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a'; j <= 'z' ; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+} 
