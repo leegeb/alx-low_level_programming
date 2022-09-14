a2eee10891383b62f790995455f5a3d08ddf6724
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-14 14:55:13 +0400

    Debugging task o

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- * main - prints if integer is positive or negative
- * Return: 0 is success
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
+        i = 98;
 	positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
