77d23e070399bc4e92f790b81cbba21035ce3ddd
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-16 02:40:50 +0400

    FizzBuzz Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-
-/**
-*main - program that prints either number
-*or fizz or buzz or fizzBuzz
-*
-*
-*Return: returns 0
-*/
+ /**
+ *main - program that prints either number,Fizz , FizzBuzz ,Buzz
+ *Return: returns 0
+ */
 
 int main(void)
 {
