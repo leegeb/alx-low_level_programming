574866c6b09b1e0baca351c3985fa92be5864e1c
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-16 01:36:45 +0400

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,9 @@
-#include < stdio.h >
 #include "main.h"
 /**
  *print_line - prints a straight line
  *@n: parameter
- *Return: 0
+ *Return: 0 is success
  */
-
 void print_line(int n)
 {
 	while (n-- > 0)
