6e45d37cbf6b3be60d1f4a6bb0c3b9a61cc4303c
Lydia Gebre <lydiakgebre@gmail.com>
2022-09-16 16:04:45 +0400

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,8 +2,6 @@
 
 /**
 *main- prints the largest prime factor
-*of a number
-*
 *Return: returns 0
 */
 
@@ -28,34 +26,3 @@ int main(void)
 	}
 	printf("%ld\n", number);
 	return (0);
-}#include <stdio.h>
-
-/**
-*main- prints the largest prime factor
-*of a number
-*
-*Return: returns 0
-*/
-
-int main(void)
-{
-	long number = 612852475143;
-	int inc;
-
-	while (inc++ < number / 2)
-	{
-	if (number % inc == 0)
-	{
-		number /= 2;
-		continue;
-	}
-
-	for (inc = 3; inc < number / 2; inc += 2)
-	{
-		if (number % inc == 0)
-			number /= inc;
-	}
-	}
-	printf("%ld\n", number);
-	return (0);
-}
