7895f127a3881e9903892ddf67aa0de7c8452223
lydia <lydiakgebre@gmail.com>
2022-09-19 08:05:11 -0700

    pointers and arrays task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n number of integers
+ * @n: number of elements
+ * @a: array
+ * Return: none
+ */
+
+void print_array(int *a, int n)
+{
+	int inc;
+
+	int inc;
+
+	for (inc = 0; inc < n ; inc++)
+	{
+		if (inc != n - 1)
+			printf("%d, ", a[inc]);
+		else
+			printf("%d", a[inc]);
+	}
+	_putchar(10);
+}
