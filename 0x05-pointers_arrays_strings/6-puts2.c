75ace5897771fc99b8b6997ecad007f27afc80bf
lydia <lydiakgebre@gmail.com>
2022-09-19 07:21:31 -0700

    pointers and arrays task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 -prints every other character of a string
+ * @str: string input
+ *
+ * Return: none
+ */
+
+void puts2(char *str)
+{
+	int i = o;
+
+	while (*(str + 1) != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
+	}
+	_putchar(10);
+}
+
