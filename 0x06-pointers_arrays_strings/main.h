a516f3a87df8cd9628e588c66cdd1f6048c741e8
lydia gebre <lydiakgebre@gmail.com>
2022-09-23 03:10:20 +0400

    task 10

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+
+/**
+ *void prototypes()
+ *int prototypes()
+ *char prototypes()
+ */
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -14,5 +21,6 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+int main(void);
 
-#endif
+#endif /*MAIN_H*/
