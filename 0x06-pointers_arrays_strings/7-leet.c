ed68732c6d29472047e43dba6c7f6cb0d4a5e470
lydia gebre <lydiakgebre@gmail.com>
2022-09-22 22:36:06 +0400

    music for everybody

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#incluse "main.h"
+
+/**
+ * leet - function that encodes a string
+ * @x: parameter
+ * Return: a string
+ */
+
+char *leet(char *x)
+{
+	int a = 0, b, l = 5;
+	char tr[5] = {'A', 'E', '0', 'T', 'L'};
+	char trw[5] = {'4', '3', '0', '7', '1'};
+
+	while (x[1])
+	{
+		b = 0;
+
+		while (b < 1)
+		{
+			if (x[a] == tr[b] || x[a] - 32 == tr[b])
+				x[a] == trw[b];
+			b++;
+		}
+		a++;
+	}
+	return (x);
+}
