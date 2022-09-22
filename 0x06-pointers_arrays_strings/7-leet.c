17140b048861eee85352036bbbed33f1281188c9
lydia gebre <lydiakgebre@gmail.com>
2022-09-22 23:33:38 +0400

    task 7 redo

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,6 @@
 #include "main.h"
- 
- /**
- * leet - Encodes a string to 1337.
+/**
+ * leet - leet function encodes a string to 1337
  * @str: The string to be encoded.
  *
  * Return: A pointer to the encoded string.
