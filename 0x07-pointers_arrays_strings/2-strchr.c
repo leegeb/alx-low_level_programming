2e6b6fb3142c288d96c9b7432894a0c1fed0523b
lydia <lydiakgebre@gmail.com>
2022-09-26 07:45:57 -0700

    character locating

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: NULL if c is not found
+ * If c is found - a pointer to the first occurance
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+	return ('\0');
+}
