888c170084951aa530a970c8be17b5b06ef40ea1
lydia <lydiakgebre@gmail.com>
2022-09-28 10:38:16 -0700

    prime check

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * is_divisible - checkes if a number is prime
+ * prime_check - checkes if a number is prime
  * @p: possible prime number
  * @d: checked number
  *
  * Return: 1 if prime, 0 if not
  */
-int is_divisible(int d, int p)
+int prime_check(int d, int p)
 {
 	if (p < 2 || p % d == 0)
 		return (0);
-	else if (d > P / 2)
+	else if (d > p / 2)
 		return (1);
-	return (is_divisible(d + 1, p));
+	return (prime_check(d + 1, p));
 }
  /**
   * is_prime_number - gives prime number
@@ -25,5 +25,5 @@ int is_prime_number(int n)
 {
 	if (n == 2)
 		return (1);
-	return (is_divisible(2, n));
+	return (prime_check(2, n));
 }
