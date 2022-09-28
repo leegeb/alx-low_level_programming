7421f1d620a0dc6661ed6afd5c78ae6c27e5cb97
lydia <lydiakgebre@gmail.com>
2022-09-28 09:48:21 -0700

    square check recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
 /**
+ * sqrt_check - checks for the square root of q
+ * @q: input number
+ * @t: guess number
+ *
+ * Return: -1 or Square root of q
+ */
+int sqrt_check(int t, int q)
+{
+	if (q * q == t)
+		return (q);
+	if (q * q > t)
+		return (-1);
+	return (sqrt_check(q + 1, t));
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: given number
  *
  * Return: -1, natural square root
  */
-
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
