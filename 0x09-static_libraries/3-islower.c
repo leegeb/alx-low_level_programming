59413bcdd24151c7097ac7f5a08ef299355e93b6
lydia <lydiakgebre@gmail.com>
2022-09-30 07:42:53 -0700

    static lib

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower -> checks for lowercase character
+ * @c: a character argument
+ * Return: returns 1 and 0 based on the condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
