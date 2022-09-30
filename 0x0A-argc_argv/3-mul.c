72964b3d4bf56202d12935ede62a2ebe6f123de8
lydia <lydiakgebre@gmail.com>
2022-09-30 05:52:25 -0700

    multiplying two integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,10 +14,10 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	if (argc == 1)
+	if (argc == 3)
 	{
 		a = atoi(argv[1]);
-		b = atoi(argv[1]);
+		b = atoi(argv[2]);
 		printf("%d\n", a * b);
 
 		return (0);
