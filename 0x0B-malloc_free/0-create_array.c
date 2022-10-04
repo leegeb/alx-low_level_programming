d989f060847e130c94f28d57139b9d74f6c68f52
lydia <lydiakgebre@gmail.com>
2022-10-04 03:30:24 -0700

    Creating an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @c: character input
+ * @size: size of the array
+ * Return: NULL if size = 0, or fails
+ * pointer to the array if success
+ */
+
+char *create_array(unsigned int size, char c);
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	s = mallock((size) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}	
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ * Description: Header files contains all prototypes
+ */
+
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+int _putchar(char c);
+
+#endif/*MAIN_H*/
