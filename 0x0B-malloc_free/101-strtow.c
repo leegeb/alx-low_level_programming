ef2ed7a8e2145a55d1c354653573063cdf80154c
lydia <lydiakgebre@gmail.com>
2022-10-04 08:42:48 -0700

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,96 @@
+#include "main.h"
+
+/**
+ * word_len - finds the length of a word
+ * @str: string to test
+ *
+ * Return: integer
+ */
+
+int word_len(char *str)
+{
+	int i = 0, len = 0;
+
+	while (*(str + i) && *(str + i) != ' ')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * word_count - counts the number of words
+ * @str: input string
+ * Return: no of words
+ */
+
+int word_count(char *str)
+{
+	int i = 0, len = 0, count = 0;
+
+	for (i = 0; *(str + i); i++)
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (*(str + i) != ' ')
+		{
+			count++;
+			i += word_len(str + i);
+		}
+	}
+	return (count);
+
+/**
+ * strtow - splits a string into words
+ * @str: string input
+ *
+ * Return: NULL if str == "",NULL or word0s
+ */
+
+char **strtow(char *str)
+{
+	int i, words, w, letters, l;
+	char **p;
+
+	if (str == NULL || str[0] == '\0')
+	{
+		return (NULL);
+	}
+	words = word_count(str);
+
+	if (words == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(char *) * (words + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < words; i++)
+	{
+		while (*(str + w) == ' ')
+		{
+			w++;
+		}
+		letters = word_len(str + w);
+		p[i] = malloc(sizeof(char) * (letters + 1));
+		if (p[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(p[i]);
+			free(p);
+			return (NULL);
+		}
+		for (l = 0; l < letters; l++)
+		{
+			p[i][l] = str[w++];
+		}
+		p[i][l] = '\0';
+	}
+	p[i] = NULL;
+	return (p);
+}
