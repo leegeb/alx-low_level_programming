2ce65e539c25dbab981f91771f4c593e7c1072e7
lydia <lydiakgebre@gmail.com>
2022-10-06 05:01:17 -0700

    correcting

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * string_nconcat - concatenates two strings
@@ -41,7 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		c[i] = s1[i];
 	}
-	for (i < (size1 + n); i++)
+	for (i = 0; i < (size1 + n); i++)
 	{
 		c[i] = s2[i - size1];
 	}
