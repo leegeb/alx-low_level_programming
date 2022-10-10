352609f4325143ca5704557d4be748bc8687e47f
lydia <lydiakgebre@gmail.com>
2022-10-10 02:03:18 -0700

    learning preprocessors

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of a file
+ * it was compiled from
+ * Return: 0 is success
+ */
+int main(void)
+{
+	_putchar("%s\n", _FILE_);
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef ABSOLUTE_NUMBER_H
+#define ABSOLUTE_NUMBER_H
+
+#define  ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+#endif /*ABSOLUTE_NUMBER_H*/
