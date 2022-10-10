aef38b2e91f70b9bee4c06288866ab086443dc70
lydia <lydiakgebre@gmail.com>
2022-10-10 16:03:04 -0700

    fixing error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,42 +14,41 @@
 dog_t *new_dog(char *name, float age, char *owner);
 {
 	int i, j, k;
-	dog_t *newdog;
+	dog_t *dog;
 
-	for (i = 0; name[i] != '\0'; i++)
-		;
-	for (j = 0; owner[j] != '\0'; j++)
-		;
-	newdog = malloc(sizeof(dog_t));
-	if (newdog == NULL)
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 	{
-		free(newdog);
 		return (NULL);
 	}
-	newdog->name = malloc(i * sizeof(newdog->name));
-	if (newdog->name == NULL)
+	for (i = 0; name[i]; i++)
+		i++;
+	dog->name = malloc(i * sizeof(dog->name));
+	if (dog->name == NULL)
 	{
-		free(newdog->name);
-		free(newdog);
+		free(dog);
 		return (NULL);
 	}
 	for (k = 0; k <= i; k++)
 	{
-		(*newdog).name[k] = name[k];
+		dog->name[k] = name[k];
 	}
-	(*newdog).age = age;
-	newdog->owner = malloc(j * sizeof(newdog->owner));
-	if (newdog->owner == NULL)
+	dog->age = age;
+	for (j = 0; owner[j] = j++)
+		j++;
+	dog->owner = malloc(j * sizeof(dog->owner));
+	if (dog->owner == NULL)
 	{
-		free(newdog->name);
-		free(newdog->owner);
-		free(newdog);
+		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
 	for (k = 0; k <= j; k++)
 	{
-		newdog->owner[k] = owner[k];
+		dog->owner[k] = owner[k];
 	}
-	return (newdog);
+	return (dog);
 }
 
