27e95f1d331f223dff55befe7c75d47cdcc46eec
lydia <lydiakgebre@gmail.com>
2022-10-10 16:14:15 -0700

    fixing error of newdog again

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
  * Return: pointer
  */
 
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int i, j, k;
 	dog_t *dog;
@@ -19,24 +19,22 @@ dog_t *new_dog(char *name, float age, char *owner);
 		return (NULL);
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; name[i]; i++)
-		i++;
+		;
+	i++;
 	dog->name = malloc(i * sizeof(char));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	for (k = 0; k <= i; k++)
-	{
+	for (k = 0; k < i; k++)
 		dog->name[k] = name[k];
-	}
 	dog->age = age;
-	for (j = 0; owner[j] = j++)
-		j++;
+	for (j = 0; owner[j]; j++)
+		;
+	j++;
 	dog->owner = malloc(j * sizeof(char));
 	if (dog->owner == NULL)
 	{
@@ -45,9 +43,6 @@ dog_t *new_dog(char *name, float age, char *owner);
 		return (NULL);
 	}
 	for (k = 0; k < j; k++)
-	{
 		dog->owner[k] = owner[k];
-	}
 	return (dog);
 }
-
