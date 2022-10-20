70cb6ad059e8386199828e11dc7ecd60f6661c9f
lydia <lydiakgebre@gmail.com>
2022-10-20 09:41:51 -0700

    list of linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - a function that prints all the elements
+ * @h: lists of list_t
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n == 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil\n)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
