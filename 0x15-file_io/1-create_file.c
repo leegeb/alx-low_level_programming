9464fa608d0fb29ac099a6ba9704fe3cde3eec45
lydia <lydiakgebre@gmail.com>
2022-10-31 16:37:31 -0700

    task 1 cm

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 	for (i = 0; text_content[i]; i++)
-		;
+
 	if (write(fd, text_content, i) == -1)
 	{
 		return (-1);
