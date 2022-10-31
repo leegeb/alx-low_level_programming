0519cb539c301359ac0bcae67689eae5c5462745
lydia <lydiakgebre@gmail.com>
2022-10-31 16:21:53 -0700

    File IO

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints
+ * it to the POSIX standard output
+ *
+ * @filename: pointer to file description
+ * @letters: num of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, bytes;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	if (letters < 1)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	bytes = read(fd, buf, letters);
+	buf[bytes + 1] = '\0';
+
+	write(STDOUT_FILENO, buf, bytes);
+	close(fd);
+	free(buf);
+	return (bytes);
+}
