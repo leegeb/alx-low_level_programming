73f2a02473c2de91b78f9b8571c224949e5f6da9
lydia <lydiakgebre@gmail.com>
2022-10-31 17:01:42 -0700

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of a file
+ * @text_content: text to append
+ *
+ * Return: 1 success, -1 is failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, i;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+		return (-1);
+	for (i = 0; text_content[i]; i++)
+		;
+	if (i == 0)
+	{
+		close(fd);
+		return (1);
+	}
+	if (write(fd, text_content, i) == -1)
+	{
+		return (-1);
+	}
+	if (close(fd) == -1)
+		return (-1);
+	return (1);
+}
